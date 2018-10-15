afacca396355ed335c2f5d49e7177672e36b268b
Aart Stuurman <aartstuurman@hotmail.com>
2018-10-16 00:18:17 +0200

    fixed projection math

--- lighting.frag
+++ lighting.frag
@@ -5,26 +5,30 @@ uniform sampler2D normalmap;
 uniform vec3 lightpos;
 uniform sampler2D positionmap;
 uniform float scaley;
+uniform vec3 position;
  
 void main()
 {
     vec3 normal, lightDir;
-	vec4 diffuse, ambient;
+	vec4 diffuse, ambient, realpos;
     float ndotl;
     float depth;
     float pixelscale = 1024/8; // 1 positionmap position unit is 128 pixels
     float distfactor;
 
+    realpos = texture2D(positionmap,gl_TexCoord[0].st);
     normal = normalize(texture2D(normalmap,gl_TexCoord[0].st).rgb);
-    lightDir = normalize(texture2D(positionmap,gl_TexCoord[0].st).rgb*pixelscale - lightpos);
+    lightDir = normalize(realpos.rgb*pixelscale - lightpos);
     distfactor = 0.01*distance(texture2D(positionmap,gl_TexCoord[0].st).rgb*pixelscale, lightpos);
     distfactor = 1.0f / (distfactor*distfactor);
     ndotl = max(dot(normal, -lightDir), 0.0);
     diffuse = texture2D(colormap,gl_TexCoord[0].st);
     gl_FragColor = vec4(distfactor*ndotl*diffuse.rgb, diffuse.a);
-    //gl_FragColor = 0.0001*vec4(ndotl*diffuse.rgb, diffuse.a) + vec4(texture2D(positionmap,gl_TexCoord[0].st).rgb, diffuse.a);
 
-    depth = texture2D(positionmap, gl_TexCoord[0].st).r;
+    //realpos = normalize(texture2D(positionmap,gl_TexCoord[0].st);
+    // depth = -y + tan(cam_angle)*rendered_z*distfactor
+    //depth = texture2D(positionmap, gl_TexCoord[0].st).r;
+    //depth = position
     gl_FragDepth = depth / 1000.0f;
     
 }
--- src/Isometric.hpp
+++ src/Isometric.hpp
@@ -11,23 +11,27 @@ class Isometric
 public:
     static sf::Vector2f RealposToScreenpos(sf::Vector2f const& position, sf::Vector2f const& cameraPos, sf::Vector2u const& screenSize)
     {
-        sf::Vector2f unitX = sf::Transform().rotate((float)std::atan(2.0f)/pi*180).transformPoint(sf::Vector2f(1.0f, 0.0f));
-        sf::Vector2f unitY = sf::Transform().rotate((float)std::atan(0.5f)/pi*180).transformPoint(sf::Vector2f(0.0f, 1.0f));
-        sf::Vector2f result(
-            position.x * unitX.x + position.y * unitX.y,
-            position.x * unitY.x + position.y * unitY.y
-        );
-        result.y *= -1.0f;
-        return result + sf::Vector2f((float)screenSize.x, (float)screenSize.y)/2.0f;
+        sf::Vector2f result = position;
+        
+        sf::Vector2f unitX(0.70710678118f, 0.70710678118f);
+        sf::Vector2f unitY(0.70710678118f, -0.70710678118f);
+        result = {
+            result.x * unitX.x + result.y * unitX.y,
+            result.x * unitY.x + result.y * unitY.y
+        };
+        result.y *= 0.5f;
+        result += sf::Vector2f((float)screenSize.x, (float)screenSize.y)/2.0f;
+        return result;
     }
 
     static sf::Vector2f ScreenposToRealpos(sf::Vector2f const& screenPos, sf::Vector2f const& cameraPos, sf::Vector2u const& screenSize)
     {
-        sf::Vector2f unitX = sf::Transform().rotate(-(float)std::atan(2.0f)/pi*180).transformPoint(sf::Vector2f(1.0f, 0.0f));
-        sf::Vector2f unitY = sf::Transform().rotate(-(float)std::atan(0.5f)/pi*180).transformPoint(sf::Vector2f(0.0f, 1.0f));
-        //sf::Vector2f centered(-1.0f, 0.0f);
         sf::Vector2f centered = screenPos - sf::Vector2f((float)screenSize.x, (float)screenSize.y)/2.0f;
-        centered.y *= -1.0f;
+        centered.y *= 2.0f;
+
+        // 0.5 sqrt(2)
+        sf::Vector2f unitX(0.70710678118f, 0.70710678118f);
+        sf::Vector2f unitY(0.70710678118f, -0.70710678118f);
         sf::Vector2f result(
             centered.x * unitX.x + centered.y * unitX.y,
             centered.x * unitY.x + centered.y * unitY.y
